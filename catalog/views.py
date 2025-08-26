eda498ed1d0569222a1323069360ec3e0c979bbd
Roman <neznayuhto@gmail.com>
2025-08-26 19:16:34 +0300

    Добавление новых контроллеров для реализации CRUD

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+
+from catalog.forms import ProductForm
 from catalog.models import Product
-from  django.views.generic import ListView, DetailView, TemplateView, View
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, TemplateView, View
+from django.urls import reverse_lazy, reverse
 
 
 class HomeView(TemplateView):
@@ -22,9 +27,33 @@ class FeedbackView(View):
         return render(request, "catalog/contacts.html")
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
 class ProductListView(ListView):
     model = Product
+    template_name = "product/product_list.html"
+    context_object_name = "product"
 
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy("catalog:product_list")
