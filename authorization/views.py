9ed1e33bf0840e08d6e2296c3d69ed1ca1166000
Roman <neznayuhto@gmail.com>
2025-08-31 23:54:24 +0300

    Настройки приложения authorization

---
+++ authorization/views.py
@@ -0,0 +1,32 @@
+import os
+from symtable import Class
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
+from django.core.mail import send_mail
+from django.contrib.auth import login
+from django.contrib.auth.views import LogoutView
+
+
+class RegisterView(CreateView):
+    template_name = 'authorization/register.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('catalog:product_list')
+
+# class CustomLogoutView(LogoutView):
+#     def get_next_page(self):
+#         return reverse_lazy('catalog:product_list')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать в наш сервис'
+        message = 'Спасибо, что зарегистрировались в нашем сервисе!'
+        from_email = os.getenv("EMAIL_HOST_USER")
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
